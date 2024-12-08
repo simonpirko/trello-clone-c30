df271d0064a4e3c4d113d7e4350b520bbcc3b46d
Simon Pirko <sipi7gm@gmail.com>
2024-12-08 22:41:30 +0300

    Initial commit

---
+++ src/main/java/by/tms/trelloclonec30/TrelloCloneC30Application.java
@@ -0,0 +1,13 @@
+package by.tms.trelloclonec30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloCloneC30Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloCloneC30Application.class, args);
+    }
+
+}
---
+++ src/test/java/by/tms/trelloclonec30/TrelloCloneC30ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tms.trelloclonec30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloCloneC30ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
