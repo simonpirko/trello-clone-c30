1bae7513d68981df1cad86f9c73ef8b1d9d4ce16
alex garag <alex.garag@bk.ru>
2024-12-17 17:27:52 +0300

    tms-122 feat Реализовал create

--- src/main/java/by/tms/trelloclonec30/service/IssueService.java
+++ src/main/java/by/tms/trelloclonec30/service/IssueService.java
@@ -4,30 +4,45 @@ import by.tms.trelloclonec30.dto.IssueCreateDto;
 import by.tms.trelloclonec30.entity.Account;
 import by.tms.trelloclonec30.entity.Issue;
 import by.tms.trelloclonec30.entity.Project;
+import by.tms.trelloclonec30.repository.AccountRepository;
 import by.tms.trelloclonec30.repository.IssueRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class IssueService {
     private final IssueRepository issueRepository;
+    private final AccountRepository accountRepository;
 
-    public IssueService(IssueRepository issueRepository) {
+    @Autowired
+    public IssueService(IssueRepository issueRepository,
+                        AccountRepository accountRepository) {
         this.issueRepository = issueRepository;
+        this.accountRepository = accountRepository;
     }
 
     public IssueCreateDto create(IssueCreateDto issueCreateDto) {
-        Account author = new Account();
-        Account assignee = new Account();
-        Project project = new Project();
         Issue issue = new Issue();
-
         issue.setTitle(issueCreateDto.getTitle());
         issue.setDescription(issueCreateDto.getDescription());
         issue.setCurrentStatus(issueCreateDto.getCurrentStatus());
+        Optional<Account> authorOpt = accountRepository.findById(Long.valueOf(issueCreateDto.getIdAuthor()));
+        if (authorOpt.isPresent()) {
+            issue.setAuthor(authorOpt.get());
+        } else {
+            throw new EntityNotFoundException("Author not found");
+        }
+        Optional<Account> assigneeOpt = accountRepository.findById(Long.valueOf(issueCreateDto.getIdAssignee()));
+        if (assigneeOpt.isPresent()) {
+            issue.setAssignee(assigneeOpt.get());
+        } else {
+            throw new EntityNotFoundException("Assignee not found");
+        }
 
-//        issue.setAuthor().setId(issueCreateDto.getIdAssignee());
-//        issue.getAuthor(issueCreateDto.getIdAuthor());
-//        issue.getProject(issueCreateDto.getIdProject());
+//        issue.setProject();
 
         issueRepository.save(issue);
         return issueCreateDto;
