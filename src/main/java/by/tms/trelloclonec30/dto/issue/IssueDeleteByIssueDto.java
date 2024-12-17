486fdd936f8ae76c9e6051548b21b025356c847c
alex garag <alex.garag@bk.ru>
2024-12-17 20:12:56 +0300

    tms-122 feat Сменил название dto

--- src/main/java/by/tms/trelloclonec30/dto/issue/IssueDeleteByIssueDto.java
+++ src/main/java/by/tms/trelloclonec30/dto/issue/IssueDeleteByIssueDto.java
@@ -5,23 +5,10 @@ import lombok.Data;
 @Data
 public class IssueDeleteByIssueDto {
     private String id;
-//    private String title;
-//    private String description;
-//    private String idAuthor;
-//    private String idAssignee;
-//    private String idProject;
-//    private Status currentStatus;
 
     @Override
     public String toString() {
         return "IssueDeleteDto{" +
-                "id='" + id + //"', " +
-//                "title='" + title + "', " +
-//                "description='" + description + "', " +
-//                "currentStatus='" + currentStatus + "', " +
-//                "idAuthor='" + idAuthor + "', " +
-//                "idAssignee='" + idAssignee + "', " +
-//                "idProject='" + idProject +
-                "'}";
+                "id='" + id + "'}";
     }
 }
